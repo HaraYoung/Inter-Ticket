9eab963d7c2de81605a282effbba487d049243af
yujinoneill <yujinoneill@gmail.com>
2022-10-26 17:57:11 +0900

    YJYJYJYJYJYJ

--- inter-ticket/src/components/TicketBox.vue
+++ inter-ticket/src/components/TicketBox.vue
@@ -8,13 +8,19 @@
     <b-col>
       <span v-if="item.status.isCanceled">예매 취소</span>
       <div v-else class="btn-flex">
-        <b-button size="sm" @click="modalEdit = !modalEdit">변경</b-button>
+        <b-button size="sm" v-b-modal.modal-multi-1>변경</b-button>
         <b-button size="sm" @click="modalCancel = !modalCancel">취소</b-button>
       </div>
     </b-col>
-    <!-- 예매 변경 모달 -->
-    <div>
-      <b-modal v-model="modalEdit" :hide-header="true">
+    <span>
+      <!-- 예매 변경 모달 -->
+      <b-modal
+        id="modal-multi-1"
+        size="lg"
+        title="First Modal"
+        ok-only
+        no-stacking
+      >
         <v-app class="ticketArea">
           <h4>{{ item.ticketName }}</h4>
           <div class="calender">
@@ -35,52 +41,50 @@
             </div>
           </div>
           <div class="ticketBtnArea">
-            <b-button class="ticketBtn" v-b-modal.modal-multi-1
+            <b-button class="ticketBtn" v-b-modal.modal-multi-2
               >변경하기</b-button
             >
-          </div>
-          <!--예매하기 버튼 클릭시 나타나는 모달-->
-          <span>
-            <!--예매 변경 모달-->
-            <b-modal
-              id="modal-multi-1"
-              size="lg"
-              title="First Modal"
-              ok-only
-              no-stacking
+            <b-button
+              size="sm"
+              variant="success"
+              @click="ok()"
+              v-b-modal.modal-multi-2
+              >확인</b-button
             >
-              <div>예매 날짜 : {{ this.picker }}</div>
-              <div>예매 매수 : {{ this.counter }}</div>
-              <div>예매 내용을 변경하시겠습니까?</div>
-              <template #modal-footer="{ ok, cancel }">
-                <b-button
-                  size="sm"
-                  variant="success"
-                  @click="ok()"
-                  v-b-modal.modal-multi-2
-                  >확인</b-button
-                >
-                <b-button size="sm" variant="danger" @click="cancel()"
-                  >취소</b-button
-                >
-              </template>
-            </b-modal>
-            <!--두번째 나타날 모달[변경 완료시]-->
-            <b-modal id="modal-multi-2" title="Second Modal" ok-only>
-              <div>예매되었습니다!</div>
-              <template #modal-footer="{ cancel }">
-                <b-button
-                  size="sm"
-                  variant="danger"
-                  @click="cancel(), onChangeUrl()"
-                  >확인</b-button
-                >
-              </template>
-            </b-modal>
-          </span>
+          </div>
+          <!--두 번째 나타날 모달[변경하기 버튼 클릭시]-->
+          <b-modal id="modal-multi-2" title="Second Modal">
+            <div>예매 날짜 : {{ this.picker }}</div>
+            <div>예매 매수 : {{ this.counter }}</div>
+            <div>예매 내용을 변경하시겠습니까?</div>
+            <template #modal-footer="{ ok, cancel }">
+              <b-button
+                size="sm"
+                variant="success"
+                @click="ok()"
+                v-b-modal.modal-multi-3
+                >확인</b-button
+              >
+              <b-button size="sm" variant="danger" @click="cancel()"
+                >취소</b-button
+              >
+            </template>
+          </b-modal>
+          <!--세 번째 나타날 모달[변경 완료시]-->
+          <b-modal id="modal-multi-3" title="Third Modal" ok-only>
+            <div>예매되었습니다!</div>
+            <template #modal-footer="{ cancel }">
+              <b-button
+                size="sm"
+                variant="danger"
+                @click="cancel(), onChangeUrl()"
+                >확인</b-button
+              >
+            </template>
+          </b-modal>
         </v-app>
       </b-modal>
-    </div>
+    </span>
     <!-- 예매 변경 모달 -->
     <!-- 예매 취소 모달 -->
     <div>
@@ -152,13 +156,15 @@ span {
 .calender {
   margin-top: 1em;
 }
+::v-deep .v-application--wrap {
+  min-height: fit-content;
+}
 </style>
 
 <script>
 export default {
   data: function () {
     return {
-      modalEdit: false,
       modalCancel: false,
       dialog: false,
       value: "",
