34e6546c8e56a051882639104284996de5c9b71c
yujinoneill <yujinoneill@gmail.com>
2022-10-28 13:59:17 +0900

    TOP 버튼 수정

--- inter-ticket/src/components/TopButComponent.vue
+++ inter-ticket/src/components/TopButComponent.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="topBtnArea" v-show="scroll" @click="onClickTop()">
     <div>▲</div>
-    <div style="font-size:12px">TOP</div>
+    <div style="font-size: 12px">TOP</div>
   </div>
 </template>
 <style scoped>
 .topBtnArea {
-  background-color: #6C757D;
+  background-color: #6c757d;
   position: fixed;
   z-index: 99999;
   bottom: 8%;
@@ -24,8 +24,8 @@
 export default {
   data() {
     return {
-        //scroll 이벤트에 따라 TOP버튼의 
-      scroll: false
+      //scroll 이벤트에 따라 TOP버튼의
+      scroll: false,
       //   scrollTop: window.scrollY,
       //   windowHeight: window.screen.availHeight,
       //   documentHeight: document.body.scrollHeight
@@ -34,23 +34,30 @@ export default {
   methods: {
     scrollEvent() {
       window.addEventListener("scroll", () => {
-        const scrollTop= window.scrollY;
+        const scrollTop = window.scrollY;
 
         const windowHeight = window.screen.availHeight; //웹브라우저의 창 높이
-        // const documentHeight= document.body.scrollHeight;   //Html 문서의 높이
+        const documentHeight = document.body.scrollHeight; //Html 문서의 높이
 
-        // console.log('스크롤이 발생하고 있음: scrollTop='+ scrollTop+
-        //        ', documentHeight='+ documentHeight+ ',windowHeight='+ windowHeight);
+        console.log(
+          "스크롤이 발생하고 있음: scrollTop=" +
+            scrollTop +
+            ", documentHeight=" +
+            documentHeight +
+            ",windowHeight=" +
+            windowHeight
+        );
 
         //일정 높이에 도달했을 때 버튼이 나타남
         if (
           //   this.scrollTop == 400 ||
           //   this.documentHeight == 2708 ||
-          windowHeight == 800
+          windowHeight >= 800 ||
+          scrollTop >= 300
         ) {
           this.scroll = true;
         }
-        if( scrollTop == 0 ){
+        if (scrollTop == 0) {
           this.scroll = false;
         }
       });
@@ -58,11 +65,11 @@ export default {
     onClickTop() {
       //let scrollTop = window.scrollY; //스크롤바의 Y좌표
       this.scroll = false;
-      window.scrollTo(0,0);
-    }
+      window.scrollTo(0, 0);
+    },
   },
   created() {
     this.scrollEvent();
-  }
+  },
 };
 </script>
